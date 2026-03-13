349257cba9896a9d47ff7c71029fb21b2433661b
Yaro <bigusyaroslav@gmail.com>
2026-03-13 09:24:55 +0200

    test: add mocks for Math.random()

--- src/tests/whyrify.test.js
+++ src/tests/whyrify.test.js
@@ -1,10 +1,18 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import { Whyrify } from "../lib/whyrify";
 
 test("it should bucket user", () => {
+    // mock value to 0.8 so Math.random() > 50%
+    vi.spyOn(Math, "random").mockReturnValue(0.8);
     const engine = new Whyrify("XXXX-XXXX-XXXX-XXXX", 50, true);
-    const bucket = engine.decide("feature-x");
-    expect(bucket).toBeOneOf(["control", "off"]);
+    const bucketX = engine.decide("feature-x");
+    expect(bucketX).toBe("control");
+    vi.restoreAllMocks();
+    // mock value to 0.2 so Math.random() < 50%
+    vi.spyOn(Math, "random").mockReturnValue(0.2);
+    const bucketY = engine.decide("feature-y");
+    expect(bucketY).toBe("off");
+    vi.restoreAllMocks();
 });
 
 test("it should bucket to off if chance is 100%", () => {
